5fb8b94095e7749c58c01e734a2425708844fc54
hilal344 <abdullahhilal344@gmail.com>
2022-11-01 13:27:36 +0530

    Task6

---
+++ main.js
@@ -0,0 +1,13 @@
+//var li = document.querySelectorAll('.list-group-item');
+//console.log(li[1]);
+//li[1].style.backgroundColor = 'green'
+
+
+//li[2].style.display = "none";
+
+
+let odd = document.querySelectorAll('li:nth-child(odd');
+
+for(let i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = 'green'
+}
