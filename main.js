d6a7ed734e94f06bf140a3987e187479aa8a1846
hilal344 <abdullahhilal344@gmail.com>
2022-11-02 17:22:28 +0530

    trial1 commit

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 let listitems = document.querySelector('#items');
-// console.log(listitems.parentElement);
-// listitems.parentElement.style.backgroundColor='red'
+console.log(listitems.parentElement);
+listitems.parentElement.style.backgroundColor='red'
 
-// console.log(listitems.lastElementChild);
-// listitems.lastElementChild.textContent = 'abdullah';
+console.log(listitems.lastElementChild);
+listitems.lastElementChild.textContent = 'abdullah';
 
-// console.log(listitems.lastChild);
-// listitems.lastChild.textContent='hilal';
+console.log(listitems.lastChild);
+listitems.lastChild.textContent='hilal';
 
-// // console.log(listitems.fir);
+// console.log(listitems.fir);
 
-// console.log(listitems.firstElementChild);
+console.log(listitems.firstElementChild);
 
-// console.log(listitems.nextSibling);
+console.log(listitems.nextSibling);
 
-// console.log(listitems.nextElementSibling);
+console.log(listitems.nextElementSibling);
 
 
-// console.log(listitems.previousSibling);
+console.log(listitems.previousSibling);
 
-// console.log(listitems.previousElementSibling);
-// listitems.previousElementSibling.textContent='redsea';
+console.log(listitems.previousElementSibling);
+listitems.previousElementSibling.textContent='redsea';
 
 var newDiv  = document.createElement('div');
 newDiv.className = 'hilal';
