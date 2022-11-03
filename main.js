813ca036e59f39eeb77706919c577bf7bd16dbf4
hilal344 <abdullahhilal344@gmail.com>
2022-11-03 12:50:48 +0530

    filter with description added

--- main.js
+++ main.js
@@ -46,8 +46,9 @@ function filterItems(e){
     var text = e.target.value.toLowerCase();
     var items = itemList.getElementsByTagName('li');
     Array.from(items).forEach(function(item){
-      var itemName = item.firstChild.textContent;
-      if(itemName.toLowerCase().indexOf(text) != -1){
+      var itemName = item.childNodes[0].textContent;
+      var itemName2 = item.childNodes[1].textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1 || itemName2.toLowerCase().indexOf(text) != -1){
         item.style.display = 'block';
       } else {
         item.style.display = 'none';
