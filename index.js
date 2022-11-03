d6338d7f3fdaf41a32a403eaa80bf3acd26dfc3e
hilal344 <abdullahhilal344@gmail.com>
2022-11-03 20:10:02 +0530

    Form with user data save facility large scalability

---
+++ index.js
@@ -0,0 +1,71 @@
+     
+        const myForm = document.querySelector('#my-form');
+        const enteredName = document.querySelector('#name').value;
+        const enteredEmail = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+
+myForm.addEventListener('submit', onSubmit);
+var itemList = document.getElementById('items');
+
+function onSubmit(e){
+       
+        e.preventDefault();
+        const enteredName = document.querySelector('#name').value;
+        const enteredEmail = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+
+
+        console.log(enteredName);
+        console.log(enteredEmail);
+        console.log(password);
+
+        localStorage.setItem('name1', enteredName);
+        localStorage.setItem('Email1', enteredEmail);
+        localStorage.setItem('password1', password);  
+
+
+        let myobj={
+            
+            name2: enteredName,
+            email2: enteredEmail,
+            password2: password,
+        }
+
+       // var objectsEntered = document.querySelector('#email').value;
+        let myobjSerialised = JSON.stringify(myobj);
+        localStorage.setItem(myobj.email2, myobjSerialised);
+        let myobjDeserialised = JSON.parse(localStorage.getItem(myobj))
+
+        var newItem = document.getElementById('name').value;
+        var newItem2 = document.getElementById('email').value
+        var newItem3 = document.getElementById('password').value;
+        var date2 = document.getElementById('date').value;
+        var time2 = document.getElementById('time').value;
+var li = document.createElement('li');
+li.className = 'list-group-item';
+li.appendChild(document.createTextNode(newItem));
+li.appendChild(document.createTextNode(' '));
+li.appendChild(document.createTextNode(newItem2));
+li.appendChild(document.createTextNode(' '));
+li.appendChild(document.createTextNode(newItem3));
+li.appendChild(document.createTextNode(' '));
+li.appendChild(document.createTextNode(date2));
+li.appendChild(document.createTextNode(' '));
+li.appendChild(document.createTextNode(time2));
+itemList.appendChild(li);
+
+
+var deleteBtn = document.createElement('button');
+deleteBtn.style="border: 2px solid red;"
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('DELETE'));
+  li.appendChild(deleteBtn);
+
+  var editbtn = document.createElement('button');
+  editbtn.style="border: 2px solid green;"
+  editbtn.className = 'btn btn-danger btn-sm float-right edit';
+  editbtn.appendChild(document.createTextNode('EDIT'));
+  li.appendChild(editbtn);
+}
+
+itemList.style.backgroundColor='white'
