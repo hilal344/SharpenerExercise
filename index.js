12bc3619fd3392a1443d2871454832ddaa60a660
hilal344 <abdullahhilal344@gmail.com>
2022-11-09 23:35:26 +0530

    this keyword question number 4, 5, 6

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-// document.getElementById("clickMe").addEventListener("click", function abc(){
-// console.log('button clicked');
-// })
+class student{
 
+    static count=0;  //static variable to call
 
+   constructor(name,age,ph_no,marks){
 
-        document.addEventListener('DOMContentLoaded', () => {
-            // let btn = document.getElementById("clickMe");
-            // btn.addEventListener('click', () => {
-            //     // handle the click event
-                console.log('DOM has loaded');
-            // });
-        });
- 
+       this.name=name;
+
+       this.age=age;
+
+       this.ph_no=ph_no;
+
+       this.marks=marks;
+
+       student.countStudent();
+
+   }
+
+   static countStudent(){
+
+       return(student.count++);     //this is how u access static variable
+
+   }
+
+   eligible(){
+
+       if(this.marks >=40){
+
+           console.log(`${this.name} age ${this.age} is eligible`);
+
+       }
+
+       else if(this.marks<40){
+
+           console.log(`${this.name} age ${this.age} is not eligible`);
+
+       }
+
+   }
+}
+
+const abdullah=new student('abdullah',18,1234,98);
+
+const hilal=new student('hilal',15,2345,55);
+
+const jaffer=new student('jaffer',16,4567,33);
+
+const sayed=new student('sayed',17,7891,17);
+
+const aarzoo=new student('aarzoo',19,3456,70);
+
+console.log(student.countStudent());
+
+abdullah.eligible();
+
+hilal.eligible();
+
+jaffer.eligible();
+
+sayed.eligible();
+
+aarzoo.eligible();
