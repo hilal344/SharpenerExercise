54c24e3a1cc76ad18618be1a648df16d68d7827b
hilal344 <abdullahhilal344@gmail.com>
2022-11-10 01:44:05 +0530

    fat arrow question number 1

--- index.js
+++ index.js
@@ -1,62 +1,27 @@
-class student{
+// let square = a => a*a;
+// console.log(square(3));
 
-    static count=0;  //static variable to call
+// let multiply = (x, y) => {return x*y};
+// console.log(multiply(3, 5));
 
-   constructor(name,age,ph_no,marks){
+// var x =function(){
+//     that = this;
+//     that.val=1;
+//     setTimeout(function(){
+//         this.val++;
+//         console.log(that.val);
+//     }, 1000)
+// }
 
-       this.name=name;
-
-       this.age=age;
-
-       this.ph_no=ph_no;
-
-       this.marks=marks;
-
-       student.countStudent();
-
-   }
-
-   static countStudent(){
-
-       return(student.count++);     //this is how u access static variable
-
-   }
-   
-   minPlacementAge(minAge){
-    return (minMarks) => {
-        if(this.age > minAge && this.marks>minMarks){
-            console.log(this.name+' is ready for the place ment');
-        }
-        else{
-            console.log(this.name+' is not ready for the place ment');
-        }
-
-    }
+// var xx = x();
 
 
-       
+// var x = function(){
+//     console.log(arguments[1]);
+// }
+// x(1, 2, 3);
 
-   }
+var x = (...n) => {
+    console.log(n[1]);
 }
-
-const abdullah=new student('abdullah',18,1234,98);
-
-const hilal=new student('hilal',15,2345,55);
-
-const jaffer=new student('jaffer',16,4567,33);
-
-const sayed=new student('sayed',17,7891,60);
-
-const aarzoo=new student('aarzoo',19,3456,70);
-
-console.log(student.countStudent());
-
-abdullah.minPlacementAge(16)(40);
-
-hilal.minPlacementAge(16)(40);
-
-jaffer.minPlacementAge(16)(40);
-
-sayed.minPlacementAge(16)(40);
-
-aarzoo.minPlacementAge(16)(40);
+x(1, 2, 3);
