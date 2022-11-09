dd738641d563b6418f18c4df44d74260ccdab3c4
hilal344 <abdullahhilal344@gmail.com>
2022-11-09 16:18:14 +0530

    call back function task 13 DOMcontent loded event listner added

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-document.getElementById("clickMe").addEventListener("click", function abc(){
-console.log('button clicked');
-})
+// document.getElementById("clickMe").addEventListener("click", function abc(){
+// console.log('button clicked');
+// })
+
+
+
+        document.addEventListener('DOMContentLoaded', () => {
+            // let btn = document.getElementById("clickMe");
+            // btn.addEventListener('click', () => {
+            //     // handle the click event
+                console.log('DOM has loaded');
+            // });
+        });
+ 
